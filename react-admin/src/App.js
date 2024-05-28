88b30ad0fbf0c1d3a46cf770ecfc086122ceac95
maya88en <maya88en@gmail.com>
2024-05-28 11:55:41 +0300

    add sidebar

---
+++ react-admin/src/App.js
@@ -0,0 +1,36 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar"
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+        <Sidebar isSidebar={isSidebar} />
+        <main className="content">
+          <Topbar setIsSidebar={setIsSidebar} />
+          
+           <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+          </Routes>
+
+        </main>
+
+        </div>
+      </ThemeProvider>
+      </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ react-admin/src/scenes/global/Sidebar.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { SubMenu, Menu, MenuItem } from "react-pro-sidebar";
+// import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+// import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+// import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+// import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  // const [selected, setSelected] = useState("Dashboard");
+    
+  return (
+  <Box
+    sx={{
+      "& .pro-sidebar-inner": {
+        background: `${colors.primary[400]} !important`
+      },
+      "& .pro-icon-wrapper": {
+        backgroundColor: "transparent !important"
+      },
+      "& .pro-inner-item": {
+        padding: "5px 35px 5px 20px !important"
+      },
+      "& .pro-inner-item:hover": {
+        color: "#868dfb !important"
+      },
+      "& .pro-menu-item.active": {
+        color: "#6870fa !important"
+      },
+    }}
+    >
+      <Sidebar collapsed={isCollapsed}>
+  <Menu iconShape="square">
+    <SubMenu label="Charts">
+    <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src={`../../assets/user.jpg`}
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                  Ahmed
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  Software Developer
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+
+
+
+
+
+      <MenuItem> Line charts </MenuItem>
+    </SubMenu>
+    <MenuItem> Documentation </MenuItem>
+    <MenuItem> Calendar </MenuItem>
+  </Menu>
+</Sidebar>;
+  </Box>
+  );
+};
+  export default Sidebar;
